2b22005b1cdaa40f07fda0f671571898fc59a3e8
Ronaldy Samias de Melo <e0335981@tjrr.local>
2025-04-28 18:02:20 -0400

    teste

---
+++ ReactNative2/my-project/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native'
+import PageInicial from '../src/components/title/'
+
+export default function App(){
+    return(
+        <View>
+            <PageInicial/>
+        </View>
+    );
+}
+
+
